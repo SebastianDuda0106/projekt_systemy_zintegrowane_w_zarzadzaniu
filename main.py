eb8e73a1847d50908e6048c439a7d8e539e8b510
bielaczek <jbielecki12@gmail.com>
2026-03-24 12:04:52 +0100

    W v2

--- main.py
+++ main.py
@@ -2,16 +2,21 @@
 from src.product import product
 from src.component import component
 def main():
-    print("Hello from projekt-systemy-zintegrowane-w-zarzadzaniu!")
+    #print("Hello from projekt-systemy-zintegrowane-w-zarzadzaniu!")
     #hello(str(input("what is your name?\n")))
-    x= tabela('acocieto')
-    for row in x.table:
-        print(row)
+    #x= tabela('acocieto')
+    #for row in x.table:
+    #    print(row)
 
-    zeszyt = product(1,2,3,10)
+    zeszyt = product(
+        production_time=1,
+        stock=20,
+        req_amount=[0,0,20,10,50,0,0],
+        weeks=7
+    )
     zeszyt.info()
-    noga = component(1,1,1,1,6,4)
-    noga.info()
+    #noga = component(1,1,1,1,6,4)
+    #noga.info()
 
 # obj tabela
     # czas realizacji
--- src/product.py
+++ src/product.py
@@ -9,6 +9,7 @@ class product:
         self.weeks = weeks
         self.createTable(weeks)
         self.product_info
+        self.fillRequiredAmount()
 
 
     def createTable(self, weeks):
@@ -16,11 +17,23 @@ class product:
         for i in range(weeks):
             list.append(0)
         self.product_info = pd.DataFrame(np.zeros((3, weeks)))
-        self.product_info.index = {
+        self.product_info = self.product_info.astype(int)
+        self.product_info.index = [
             'Przewidywany popyt',
             'Produkcja',
             'Dostępne'
-            }
+            ]
         
+    def fillRequiredAmount(self):
+        if(len(self.req_amount) == self.weeks):
+            self.product_info.iloc[0] = self.req_amount
+        else:
+            print('wrong size of req_amount list')
+    
     def info(self):
-        print(self.product_info,'\n')
+        print(self.product_info)
+        print('Czas realizacji    ' ,self.production_time)
+        print('Na stanie          ' ,self.stock)
+        
+        #self.product_info.loc['Przewidywany popyt',2] = 30
+        #print(self.product_info.loc['Przewidywany popyt',2])
