e52ea092a10f5fad27dc5a0f178fbb2056e92d5f
Sebastian Duda <sebastian777duda@outlook.com>
2026-04-15 12:16:37 +0200

    obsługa błędu odczytu i poprawka czyszczenia konsoli

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 #Autorzy: Sebastian Duda, Jan Bielecki
 logi_global=True
 
-
 if logi_global:
-    print("wczytywanie bibliotek")
+    print("wczytywanie bibliotek (1/4)")
 from src.product import product
+if logi_global:
+    print("wczytywanie bibliotek (2/4)")
 from src.component import component
+if logi_global:
+    print("wczytywanie bibliotek (3/4)")
 from src.menu import menu,menup,lista2_1_2_r,lista2_1_2_tyg
+if logi_global:
+    print("wczytywanie bibliotek (4/4)")
 import os
+
+clear_console=lambda: os.system('clear')
 
 def edycja_mrp(tabela,tryb=0,logi=False):
     tabela.info()
@@ -53,24 +60,24 @@ def edycja_mrp(tabela,tryb=0,logi=False):
                 case 8:wiersz='Wielkość parii'
                 case 9:wiersz='Na stanie'
                 case 10:wiersz='Ilość w BOM'
-            if not logi:os.system('cls')
+            if not logi:clear_console()
             tabela.info()
             if wybor in range(0,7):
                 #lista2_1_t=[''] #tutaj wpisać listę z numerami tygodni
                 for kolumna in range(1,tabela.weeks+1):
-                    if not logi:os.system('cls')
+                    if not logi:clear_console()
                     tabela.info()
                     user_input='start'
                     while not(isinstance(user_input,int)):
                         try:
                             user_input=int(input(f'\n{wiersz}, wprowadź nową wartość dla tygodnia {kolumna} : '))
                         except:
-                            if not logi:os.system('cls')
+                            if not logi:clear_console()
                             tabela.info()
                             print('wprowadź poprawną liczbę')
                     tabela.product_info.loc[wiersz,kolumna]=user_input
             if wybor in range(7,11):
-                if not logi:os.system('cls')
+                if not logi:clear_console()
                 tabela.info()
                 user_input='start'
                 while not(isinstance(user_input,int)):
@@ -95,27 +102,27 @@ def edycja_mrp(tabela,tryb=0,logi=False):
                 #lista2_1_t=[''] #tutaj wpisać listę z numerami tygodni
                 kolumna=menup(list(map(lambda x:x,range(1,tabela.weeks+1))),f'wprowadź numer tygodnia od 1 do {tabela.weeks}:\n')
                 for wiersz in lista2_1_2_r[0:6]:
-                    if not logi:os.system('cls')
+                    if not logi:clear_console()
                     tabela.info()
                     user_input='start'
                     while not(isinstance(user_input,int)):
                         try:
                             user_input=int(input(f'wprowadź nową wartość do "{wiersz}":\n'))
                         except:
-                            if not logi:os.system('cls')
+                            if not logi:clear_console()
                             tabela.info()
                             print('wprowadź poprawną liczbę')
                     tabela.product_info.loc[wiersz,kolumna]=user_input
             if wybor in range(2,6):
                 kolumna=1
-                if not logi:os.system('cls')
+                if not logi:clear_console()
                 tabela.info()
                 user_input='start'
                 while not(isinstance(user_input,int)):
                     try:
                         user_input=int(input(f'wprowadź wartość liczbową: '))
                     except:
-                        if not logi:os.system('cls')
+                        if not logi:clear_console()
                         tabela.info()
                         print('wprowadź poprawną liczbę')
 
@@ -124,13 +131,14 @@ def edycja_mrp(tabela,tryb=0,logi=False):
             elif wybor == 4: tabela.stock          =tabela.product_info.loc[wiersz,kolumna]=user_input
             elif wybor == 5: tabela.req_amount     =tabela.product_info.loc[wiersz,kolumna]=user_input
     
-    if not logi:os.system('cls')
+    if not logi:clear_console()
     print("tabela po edycji")
     tabela.info()
     input("Naciśnij enter aby kontynuować\n")
 
 
 def main(logi=False):
+
     
     if logi:print("wczytywanie wartości domyślnych GHP")
     
@@ -202,70 +210,70 @@ def main(logi=False):
     run=1
     while run==1:
         #menu główne
-        if not logi:os.system('cls')
+        if not logi:clear_console()
         if saymessage:
             print(message)
             saymessage=False
         match (menu(lista1)):
             case 1:#wyswietlanie
-                if not logi:os.system('cls')
+                if not logi:clear_console()
                 match(menu(lista1_1)):
                     case 1:#wszystko
                         if logi == True:
                             print("informacje zeszyt")
-                        else:os.system('cls')
+                        else:clear_console()
                         zeszyt.info()
                         input("Naciśnij enter aby kontynuować\n")
                         
                         if logi == True:
                             print("informacje papier")
-                        else:os.system('cls')
+                        else:clear_console()
                         papier.info()
                         input("Naciśnij enter aby kontynuować\n")
                         
                         if logi == True:
                             print("informacje okladka")
-                        else:os.system('cls')
+                        else:clear_console()
                         okladka.info()
                         input("Naciśnij enter aby kontynuować\n")
 
                         if logi == True:
                             print("informacje skora")
-                        else:os.system('cls')
+                        else:clear_console()
                         skora.info()
                         input("Naciśnij enter aby kontynuować\n")
                     case 2:#GHP
                         if logi == True:
                             print("informacje zeszyt")
-                        else:os.system('cls')
+                        else:clear_console()
                         zeszyt.info()
                         input("Naciśnij enter aby kontynuować\n")
                     case 3:#MRP
-                        if not logi:os.system('cls')
+                        if not logi:clear_console()
                         match(menu(lista1_1_2)):
                             case 1:#papier
                                 if logi == True:
                                     print("informacje zeszyt")
-                                else:os.system('cls')
+                                else:clear_console()
                                 papier.info()
                                 input("Naciśnij enter aby kontynuować\n")
                             case 2:#okladka 
                                 if logi == True:
                                     print("informacje okladka")
-                                else:os.system('cls')
+                                else:clear_console()
                                 okladka.info()
                                 input("Naciśnij enter aby kontynuować\n")
                             case 3:#skora
                                 if logi == True:
                                     print("informacje skora")
-                                else:os.system('cls')
+                                else:clear_console()
                                 skora.info()
                                 input("Naciśnij enter aby kontynuować\n")
             case 2:#edytowanie
-                if not logi:os.system('cls')
+                if not logi:clear_console()
                 match(menu(lista2_1)):
                     case 1:#GHP
-                        if not logi:os.system('cls')
+                        if not logi:clear_console()
                         zeszyt.info()
                         match(tryb_edycji):
                             case 0:#wybór szczegółowy
@@ -302,20 +310,20 @@ def main(logi=False):
                                 if wybor in range(0,4):
                                     #lista2_1_t=[''] #tutaj wpisać listę z numerami tygodni
                                     for kolumna in range(1,zeszyt.weeks+1):
-                                        if not logi:os.system('cls')
+                                        if not logi:clear_console()
                                         zeszyt.info()
                                         user_input='start'
                                         while not(isinstance(user_input,int)):
                                             try:
                                                 user_input=int(input(f'\n{wiersz}, wprowadź nową wartość dla tygodnia {kolumna} : '))
                                             except:
-                                                if not logi:os.system('cls')
+                                                if not logi:clear_console()
                                                 zeszyt.info()
                                                 print('wprowadź poprawną liczbę')
                                         zeszyt.product_info.loc[wiersz,kolumna]=user_input
                                 if wybor in range(4,6):
                                     kolumna=1
-                                    if not logi:os.system('cls')
+                                    if not logi:clear_console()
                                     zeszyt.info()
                                     user_input='start'
                                     while not(isinstance(user_input,int)):
@@ -336,52 +344,52 @@ def main(logi=False):
                                     #lista2_1_t=[''] #tutaj wpisać listę z numerami tygodni
                                     kolumna=menup(list(map(lambda x:x,range(1,zeszyt.weeks+1))),f'wprowadź numer tygodnia od 1 do {zeszyt.weeks}:\n')
                                     for wiersz in lista2_1_1_r[0:3]:
-                                        if not logi:os.system('cls')
+                                        if not logi:clear_console()
                                         zeszyt.info()
                                         user_input='start'
                                         while not(isinstance(user_input,int)):
                                             try:
                                                 user_input=int(input(f'wprowadź nową wartość do "{wiersz}":\n'))
                                             except:
-                                                if not logi:os.system('cls')
+                                                if not logi:clear_console()
                                                 zeszyt.info()
                                                 print('wprowadź poprawną liczbę')
                                         zeszyt.product_info.loc[wiersz,kolumna]=user_input
                                 if wybor in range(2,4):
                                     kolumna=1
-                                    if not logi:os.system('cls')
+                                    if not logi:clear_console()
                                     zeszyt.info()
                                     user_input='start'
                                     while not(isinstance(user_input,int)):
                                         try:
                                             user_input=int(input(f'wprowadź wartość liczbową: '))
                                         except:
-                                            if not logi:os.system('cls')
+                                            if not logi:clear_console()
                                             zeszyt.info()
                                             print('wprowadź poprawną liczbę')
                                 if wybor == 2: zeszyt.product_info.loc[wiersz,kolumna]=zeszyt.production_time=user_input
                                 elif wybor == 3: zeszyt.product_info.loc[wiersz,kolumna]=zeszyt.stock=user_input
                         
-                        if not logi:os.system('cls')
+                        if not logi:clear_console()
                         print("tabela po edycji")
                         zeszyt.info()
                         input("Naciśnij enter aby kontynuować\n")
 
-                        if not logi:os.system('cls')
+                        if not logi:clear_console()
                         zeszyt.info()
                     
                     case 2:#MRP
                         #listy dla MRP
-                        if not logi:os.system('cls')
+                        if not logi:clear_console()
                         match(menu(lista2_1_2)):
                             case 1:#papier
-                                if not logi:os.system('cls')
+                                if not logi:clear_console()
                                 edycja_mrp(papier,tryb_edycji)
                             case 2:#okladka
-                                if not logi:os.system('cls')
+                                if not logi:clear_console()
                                 edycja_mrp(okladka,tryb_edycji)
                             case 3:#skora
-                                if not logi:os.system('cls')
+                                if not logi:clear_console()
                                 edycja_mrp(skora,tryb_edycji)
             case 3:#przeliczanie
                 if logi == True:print("przeliczanie wartości w tabelach")
@@ -409,16 +417,20 @@ def main(logi=False):
                 saymessage=True
                 message='zapisano do pliku xls'
             case 5:#odczyt
-                zeszyt.readXLS()
-                papier.readXLS()
-                okladka.readXLS()
-                skora.readXLS()
-                saymessage=True
-                message='odczytano z pliku xls'
+                try:
+                    zeszyt.readXLS()
+                    papier.readXLS()
+                    okladka.readXLS()
+                    skora.readXLS()
+                    saymessage=True
+                    message='odczytano z pliku xls'
+                except:
+                    saymessage=True
+                    message='NIE odczytano z pliku xls!'
             case 6:#ustawienia
                 setting=True
                 while setting:
-                    if not logi:os.system('cls')
+                    if not logi:clear_console()
                     lista6_1=[f'Zmień Tryb edycji (obecnie: {tryb_edycji+1} - {lista6_1_t[tryb_edycji]})\n 1 - szczegółowy(wiersz,kolumna),\n 2 - cały wiersz,\n 3 - cały tydzień','wróć']
                     wybor=menu(lista6_1)
                     match(wybor):
